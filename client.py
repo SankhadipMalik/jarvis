2a58d39bb20e49d9205a94cfee4a63a27031a566
SankhadipMalik <sankhadipm0@gmail.com>
2025-12-30 22:37:49 +0530

    Replace hardcoded API key with placeholder
    Updated API key placeholder for security.

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 # pip install openai 
 # if you saved the key under a different environment variable name, you can do something like:
 client = OpenAI(
-  api_key="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",
+  api_key="<your api key>",
 )
 
 completion = client.chat.completions.create(
@@ -13,5 +13,6 @@ completion = client.chat.completions.create(
     {"role": "user", "content": "what is coding"}
   ]
 )
+
 
 print(completion.choices[0].message.content)
